47e996fe71cef0fad6873309e1d2d3206fc66917
unknown <amf101@gcloud.ua.es>
2022-08-04 19:03:46 +0200

    api login controller

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -98,70 +98,7 @@ function ValidateUser(user){
             }
         })
 
-/*
-        var sqlQueryAdmin = "SELECT * FROM administradores WHERE gmail=?";
-        conn.query(sqlQueryAdmin, [user.email], (error,result,fields) => {
-            if(error){
-                dbox(error);
-            }else{
-                if(result.length > 0){
-                    var contraseñaAdmin = result[0]["contraseña"]; 
-                    console.log(contraseñaAdmin) ;
-                    console.log(user.contraseña);
-                    bcrypt.compare(user.contraseña, contraseñaAdmin, (err, coinciden) => {
-
-                        if (err) {
-                            dbox(err);
-                        } else {
-                            if (coinciden) {
-                                localStorage.setItem('admin', user.email);
-                                window.location.href = "../html/adminMain.html";
-                            } else {
-                                dbox('La contraseña es incorrecta');
-                            }
-                        }
-                    });
-
-                }else{
-                    var sqlQuery = "SELECT * FROM usuarios WHERE email=?"
-                    conn.query(sqlQuery, [user.email], (error,result,fields) => {
-                        if(error){
-                            dbox(error);
-                        }else{
-                            if(result.length > 0){
-                                var contraseñaDB = result[0]["contraseña"]; 
-                                console.log(contraseñaDB) ;
-                                bcrypt.compare(user.contraseña, contraseñaDB, (err, coinciden) => {
-                                    if (err) {
-                                        dbox(err);
-                                    } else {
-                                        if (coinciden) {
-                                            localStorage.setItem('user', user.email);
-                                            localStorage.setItem('altura', result[0].altura);
-                                            localStorage.setItem('peso', result[0].peso);
-                                            localStorage.setItem('edad', result[0].edad);
-                                            localStorage.setItem('sexo', result[0].sexo);
-                                            localStorage.setItem('nombre', result[0].nombre);
-                                            localStorage.setItem('tipo', result[0].tipoAlimentacion);
-                                            //COGER ALIMENTOS?
-
-                                            window.location.href = "../html/sessionMain.html";
-                                        }
-                                        else {
-                                            dbox('La contraseña es incorrecta');
-                                        }
-                                    }
-                                });
-                            }else{dbox('El email no esta registrado en nuestra base de datos');}        
-                        }
-                    });
-                }
-            }
-        });
-*/
-
-        
-                         
+                             
         }catch(error){
             console.log(error);
         }
